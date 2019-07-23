2350b58c23aefe0daf669cb0fce76524842e701b
renzoafable <lbafable@up.edu.ph>
2019-07-23 10:54:20 +0800

    Add initial projects section

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-  $('.nav-link, .navbar-brand').click(function() {
-    var sectionTo = $(this).attr('href');
-    $('html, body').animate(
-      {
-        scrollTop: $(sectionTo).offset().top
-      },
-      1500
-    );
+  $('a[data-smooth]').click(function() {
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+        return false;
+      }
+    }
   });
 });
